3d1e87af8f4f84975e9ccb41b0073165f2469bce
dd <dqrd123@gmail.com>
2020-06-18 22:17:11 +0900

    feat: get Client Location & Weather

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import { Loading } from './component/Loading';
+import * as Location from "expo-location";
+import axios from 'axios';
 
-export default function App() {
+const API_KEY = "3147da487cd903f92848f0c5f86f1eee";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const getWeather = async (latitude, longitude) => {
+    const response = await axios.post("http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+API_KEY);
+    console.log(response);
+  }
+
+  useEffect(() => {
+    const getPosition = async () => {
+      try {
+        await Location.requestPermissionsAsync();
+        const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+        getWeather(latitude, longitude);
+        setIsLoading(false);
+      }
+      catch (error) {
+        Alert.alert("Can't find you.", "So sad");
+      }
+    };
+    getPosition();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    isLoading ? <Loading/> : null
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
